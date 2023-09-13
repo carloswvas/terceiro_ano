fc81282fa6a629e5031d40bbe4939687c21fdb67
Carlos Vasconcelos <81450652+carloswvas@users.noreply.github.com>
2023-09-13 09:02:53 -0300

    Add files via upload

---
+++ ORM_SEQUELIZE/6_excluindo_dados/db/conn.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize('nodesequelize','root', 'root', {
+    host: '127.0.0.1',
+    port:3306,
+    dialect: 'mysql'
+})
+
+// try{
+//     sequelize.authenticate()
+//     console.log('Conectado com sucesso')
+// }catch(err){
+//     console.log('Não foi possível conectar: '+err)
+// }
+
+module.exports = sequelize
---
+++ ORM_SEQUELIZE/6_excluindo_dados/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const conn = require("./db/conn");
+
+const User = require("./models/User");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+//Mostrar o formuário
+app.get("/users/create", (request, response) => {
+  return response.render("adduser");
+});
+
+//Cadastrar as informações do formulário
+app.post("/users/create", async (request, response) => {
+  const { name, occupation } = request.body;
+  let newsletter = request.body.newsletter;
+
+  if (newsletter === "on") {
+    newsletter = true;
+  } else {
+    newsletter = false;
+  }
+  console.log(request.body);
+  //Inserir
+  await User.create({ name, occupation, newsletter });
+
+  return response.redirect("/");
+});
+
+//Rota para listar um único usuário
+app.get("/users/:id", async function (request, response) {
+  const id = request.params.id;
+  //SELECT * FROM users WHERE id = id
+  const user = await User.findOne({ raw: true, where: { id: id } });
+  console.log(user)
+  return response.render('viewuser', {user})
+});
+
+app.post('/users/delete/:id', async(request, response)=>{
+  const id = request.params.id
+  //Delete - destroy
+  await User.destroy({where: {id: id}})
+  return response.redirect('/')
+})
+
+app.get("/", async (request, response) => {
+  const users = await User.findAll({ raw: true });
+  // console.log(users)
+  return response.render("home", { users });
+});
+
+conn
+  .sync()
+  .then(() => {
+    app.listen(3333, () => {
+      console.log("Servidor Online");
+    });
+  })
+  .catch((error) => console.log(error));
